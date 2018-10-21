d02c78b66ccfd60b2a04c7e0996531bd34966de1
Sriram Baireddy <sbairedd@purdue.edu>
2018-10-20 21:15:58 -0400

    Making a more useful view. However, it still needs to be hardcoded

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 def index(request):
-    return HttpResponse("Hello world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 
 def detail(request, question_id):
@@ -12,6 +16,7 @@ def detail(request, question_id):
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
+
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
