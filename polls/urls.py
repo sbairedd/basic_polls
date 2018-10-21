2f6bb86ed7c8ab7ddcf0466b85551b2ed77b5b5e
Sriram Baireddy <sbairedd@purdue.edu>
2018-10-20 20:59:28 -0400

    Update urls to reflect new views for polls

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-     url(r'^$', views.index, name='index'),
+    # /polls/
+    url(r'^$', views.index, name='index'),
+
+    # /polls/7
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+
+    # /polls/7/results/
+    url(r'^(P<question_id>[0-9]+)/results/$', views.results, name='results'),
+
+    # /polls/7/vote/
+    url(r'^(P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
